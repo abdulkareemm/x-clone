adb637f9c0f8f5abbbd9aced8c23d1e809745920
abdulkareemm <eng.kreem.96@gmail.com>
2024-04-26 15:15:15 +0300

    design sign-up login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home"
+import Login from "./pages/auth/login/Login";
+import SignUp from "./pages/auth/signup/Signup";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      main
-    </>
-  )
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
